ee675834c6b321898fc5c3a6433c3cdd13e939c5
zeyad alaa <zeyadalaa1225@gmail.com>
2024-10-18 00:11:53 +0300

    chatpage-dummyrecieve-filesseparation

---
+++ whisper/lib/components/icon-creation.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class IconCreationWidget extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color color;
+  final Function onTap;
+
+  const IconCreationWidget({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.color,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onTap(),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            child: Icon(
+              icon,
+              size: 29,
+              color: Colors.white,
+            ),
+            backgroundColor: color,
+          ),
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
