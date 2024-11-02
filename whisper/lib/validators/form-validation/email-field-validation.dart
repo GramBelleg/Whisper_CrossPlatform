cf2dedb9ace9e1425185cd4e5de83cfc268bc4fd
Seif-abdelbaky <seifabdelbaky2211@gmail.com>
2024-11-02 19:58:22 +0200

    Added to email validations

--- whisper/lib/validators/form-validation/email-field-validation.dart
+++ whisper/lib/validators/form-validation/email-field-validation.dart
@@ -3,9 +3,12 @@ String? ValidateEmailField(String? data) {
     return 'This field is required';
   }
 
+  if (data.length > 50) {
+    return 'Email cannot exceed 50 characters';
+  }
+
   final emailRegex = RegExp(
-      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-  );
+      r'^(?!.*\.\.)(?!.*\.$)(?!^\.)([a-zA-Z0-9._%+-]+)(?<!\.)@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
 
   if (!emailRegex.hasMatch(data)) {
     return 'Enter a valid email';
