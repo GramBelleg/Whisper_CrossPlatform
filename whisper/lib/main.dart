5d2bab48e979815f8a8cc7957483d0a83e40061f
g-magdy <george.hanna03@eng-st.cu.edu.eg>
2024-11-04 12:52:45 +0200

    feat: blocked users ui and qubit

--- whisper/lib/main.dart
+++ whisper/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:whisper/cubit/blocked_users_cubit.dart';
 import 'package:whisper/cubit/messages-cubit.dart';
 import 'package:whisper/cubit/visibility_cubit.dart';
 import 'package:whisper/pages/confirmation-code.dart';
@@ -23,6 +24,7 @@ void main() {
             MessagesCubit(ChatViewModel(), ChatDeletionService())),
     // BlocProvider(create: (context) => UserCubit()),
     BlocProvider(create: (context) => VisibilityCubit()),
+    BlocProvider(create: (context) => BlockedUsersCubit()),
   ], child: Whisper()));
 }
 
--- whisper/lib/pages/blocked-users.dart
+++ whisper/lib/pages/blocked-users.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:whisper/cubit/blocked_users_cubit.dart';
 import '../constants/colors.dart';
 
 class BlockedUsersPage extends StatefulWidget {
@@ -12,21 +14,39 @@ class _BlockedUsersPageState extends State<BlockedUsersPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: firstNeutralColor,
-      appBar: AppBar(
-        title: Text(
-          "Blocked Users",
-          style: TextStyle(color: primaryColor),
-        ),
         backgroundColor: firstNeutralColor,
-        iconTheme: IconThemeData(color: primaryColor),
-      ),
-      body: Center(
-        child: Text(
-          "Blocked Users Content",
-          style: TextStyle(color: secondNeutralColor, fontSize: 24),
+        appBar: AppBar(
+          title: Text(
+            "Blocked Users",
+            style: TextStyle(color: primaryColor),
+          ),
+          backgroundColor: firstNeutralColor,
+          iconTheme: IconThemeData(color: primaryColor),
         ),
-      ),
-    );
+        body: BlocBuilder<BlockedUsersCubit, List<Map<String, String>>>(
+          builder: (context, blockedUsers) {
+            return ListView.builder(
+              itemCount: blockedUsers.length,
+              itemBuilder: (context, index) {
+                final user = blockedUsers[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(user['profilePicture']!),
+                  ),
+                  title: Text(
+                    user['name']!,
+                    style: TextStyle(color: secondNeutralColor),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.close, color: secondNeutralColor),
+                    onPressed: () => context
+                    .read<BlockedUsersCubit>()
+                    .unblockUser(index),
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
