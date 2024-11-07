0063810e6e5401902d92f8ba33475e1be1224503
Karim-Agami <Karim.mohamed702.km@gmail.com>
2024-11-07 21:39:15 +0200

    added forget password test and removed some of the invalid testcases

--- whisper/integration_test/utils/test_cases/login-test-cases.dart
+++ whisper/integration_test/utils/test_cases/login-test-cases.dart
@@ -14,7 +14,6 @@ final Map<String, List<String>> loginTestCases = {
     'user@.com', // Missing local part
     'user@domain.com@domain.com', // Multiple '@'
     'user@domain..com', // Invalid consecutive dots
-    'k@gmail.comasd', // Invalid characters
     '${generateLargeString(100)}@domain.com', // Excessively long local part
   ],
   'Invalid Passwords': [
@@ -34,6 +33,7 @@ final Map<String, List<String>> loginTestCases = {
     '123456@', // Digits and special character only
     'Ab1!', // Too short but valid structure
     'kKa1_@ahsh', // Invalid special character
+    'Whi@perrrr123', // repeated characters
     '${generateLargeString(100)}@A1a', // Excessively long local part
   ],
   'Valid but Not Existing Emails': [
@@ -42,6 +42,7 @@ final Map<String, List<String>> loginTestCases = {
     'test@sub.domain.com',
     'test@domain.corporate',
     'test+education@domain.corporate',
+    'karim.agami.ext@org.com',
   ],
 };
 
